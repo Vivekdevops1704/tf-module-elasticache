361abc3c00a72208a21e47a7a381cbb21a8d8e97
vivek <vivekemails17@gmail.com>
2023-12-04 17:22:09 +0530

    all add

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "env" {}
+variable "tags" {}
+variable "subnet_ids" {}     
+variable "sg_ingress_cidr" {}
+variable "vpc_id" {}
+variable "subnet_ids" {}
+variable "elasticache_type" {}
+variable "family" {}
+variable "port" {}
+variable "engine" {}
+variable "node_type"{}
+variable "num_cache_nodes" {}
+variable "engine_version" {}
